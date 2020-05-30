2b870020b42b89b46f11c217ef9340c9798ae61a
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-30 12:30:06 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Actor } from './Model/Actor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngfor';
+  
+  public actores: Actor[];
+  constructor() {
+    this.actores = [
+      {
+         actor_name: 'Peter Dinklage',
+         character_name: 'Tyrion Lannister',
+         gender: 'Male',
+         status: 'Alive'
+      },
+      {
+         actor_name: 'Sean Bean',
+         character_name: 'Ned Stark',
+         gender: 'Male',
+         status: 'Dead'
+      },
+      {
+         actor_name: 'Emilia Clark',
+         character_name: 'Khaleesi',
+         gender: 'Female',
+         status: 'Alive'
+      },
+      {
+         actor_name: 'Catelyn Stark',
+         character_name: 'Michelle Fairley',
+         gender: 'Female',
+         status: 'Dead'
+      }
+    ];
+  }
+
 }
